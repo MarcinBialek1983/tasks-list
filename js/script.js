05b1f7477a608430422b007a5e478eefab63a0cd
Marcin Bialek <test-git@youcode.pl>
2022-12-21 13:53:55 +0100

    adding new task and showing when the task are done

--- js/script.js
+++ js/script.js
@@ -12,18 +12,40 @@
 
     const render = () => {
         let htmlString = "";
-        for (const task of tasks ) {
+        for (const task of tasks) {
             htmlString += `
-            <li>
+            <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}"
+            >
+            <button class="js-done">Zrobione ?</button>
+            <button class="js-remove">Usuń</button>
             ${task.content}
             </li>
             `;
         }
-        document.querySelector(".js-tasks"). innerHTML = htmlString;
-    } 
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+ const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    if (newTaskContent === "") {
+        return;
+    }
+
+    addNewTask(newTaskContent);
+}
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
     };
     init();
 }
